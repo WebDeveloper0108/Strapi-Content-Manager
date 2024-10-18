92534e78355029bae8773a3539d5173c72e99e96
WebDeveloper0108 <masterofwebdev@gmail.com>
2024-10-18 23:16:09 +0530

    files

---
+++ src/Navbar.jsx
@@ -0,0 +1,25 @@
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./Context";
+import Navlinks from "./Navlinks";
+
+const Navbar = () => {
+  const { openSidebar } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
+  return (
+    <nav onMouseOver={handleSubmenu}>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        <Navlinks />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
